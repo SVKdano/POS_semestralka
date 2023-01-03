2d9660466a7d53a45b3d6aa1c1c8f13a52c02af2
Daniel Lieskovsky <danol147@gmail.com>
2023-01-03 19:36:07 +0100

    git ignore added

--- cmake-build-debug/CMakeFiles/Bulanci.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Bulanci.dir/DependInfo.cmake
@@ -8,9 +8,9 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/Andrej/CLionProjects/POS_semestralka/cpp/block.cpp" "CMakeFiles/Bulanci.dir/cpp/block.cpp.obj" "gcc" "CMakeFiles/Bulanci.dir/cpp/block.cpp.obj.d"
-  "C:/Users/Andrej/CLionProjects/POS_semestralka/cpp/game.cpp" "CMakeFiles/Bulanci.dir/cpp/game.cpp.obj" "gcc" "CMakeFiles/Bulanci.dir/cpp/game.cpp.obj.d"
-  "C:/Users/Andrej/CLionProjects/POS_semestralka/main.cpp" "CMakeFiles/Bulanci.dir/main.cpp.obj" "gcc" "CMakeFiles/Bulanci.dir/main.cpp.obj.d"
+  "C:/CLionProjects/Bulanci/cpp/block.cpp" "CMakeFiles/Bulanci.dir/cpp/block.cpp.obj" "gcc" "CMakeFiles/Bulanci.dir/cpp/block.cpp.obj.d"
+  "C:/CLionProjects/Bulanci/cpp/game.cpp" "CMakeFiles/Bulanci.dir/cpp/game.cpp.obj" "gcc" "CMakeFiles/Bulanci.dir/cpp/game.cpp.obj.d"
+  "C:/CLionProjects/Bulanci/main.cpp" "CMakeFiles/Bulanci.dir/main.cpp.obj" "gcc" "CMakeFiles/Bulanci.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
