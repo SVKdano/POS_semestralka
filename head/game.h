9af5097930cae7ffff5b1f2cfb232e67e3a10256
Daniel Lieskovsky <danol147@gmail.com>
2023-01-03 14:27:54 +0100

    rendered player and enemy as rectangular

--- head/game.h
+++ head/game.h
@@ -2,9 +2,15 @@
 // Created by Daniel Lieskovský on 3. 1. 2023.
 //
 
+
 #pragma once
+#include <SFML/Graphics.hpp>
 
 class Game {
+    private:
+        sf::RectangleShape player;
+        sf::RectangleShape enemy;
+
     public:
         Game();
         void renderWindow();
