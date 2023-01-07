86f6cd92adf19971ab259586a7eff409a862fe3c
Andrej-Dusa <ayba1dusa@gmail.com>
2023-01-07 12:41:34 +0100

    -cleaned main

--- main.cpp
+++ main.cpp
@@ -1,47 +1,47 @@
-
-
 #include "head/game.h"
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <cstdlib>
 
-sf::Context context;
 Game game;
 
-void render(bool * isUpdated, std::mutex *mut, std::condition_variable * render, std::condition_variable * update, sf::RenderWindow *window) {
+void render(std::condition_variable * renderAll, std::condition_variable * notify, sf::RenderWindow *window, std::mutex *mutex, bool * updating) {
     while(game.isWindowOpened()) {
         window->setActive(true);
-        std::unique_lock<std::mutex> lock(* mut);
-        while (!(*isUpdated)) render->wait(lock);
+        std::unique_lock<std::mutex> lock(* mutex);
+
+        while (!(*updating)) renderAll->wait(lock);
+
         game.renderWindow();
-        (*isUpdated) = false;
-        update->notify_one();
+        (*updating) = false;
+        notify->notify_one();
     }
 }
 
-void update(bool * isUpdated, std::mutex *mut, std::condition_variable * render, std::condition_variable * update) {
+void update(std::condition_variable * renderAll, std::condition_variable * notify, std::mutex *mutex, bool * updating) {
     while(game.isWindowOpened()) {
-        std::unique_lock<std::mutex> lock(*mut);
-        while (*isUpdated) update->wait(lock);
+        std::unique_lock<std::mutex> lock(*mutex);
+
+        while (*updating) notify->wait(lock);
+
         game.updateWindow();
-        (*isUpdated) = true;
-        render->notify_one();
+        (*updating) = true;
+        renderAll->notify_one();
     }
 }
 
 int main()
 {
-    std::cout << "som tu" << std::endl;
     putenv("DISPLAY=192.168.0.143");
-    //setenv("DISPLAY","192.168.0.104:7.0",true);
-    std::mutex mut;
-    std::condition_variable rend, upd;
-    bool isUpdated = false;
+    std::condition_variable renderAll, notify;
+    std::mutex mutex;
+    bool updating = false;
+
     game.getWindow()->setActive(false);
-    std::thread t1(render,&isUpdated, &mut,&rend, &upd, game.getWindow());
-    update(&isUpdated, &mut,&rend, &upd);
-    t1.join();
+    std::thread thread(render, &renderAll, &notify, game.getWindow(), &mutex, &updating);
+    update(&renderAll, &notify, &mutex, &updating);
+    thread.join();
+
     return 0;
 }
-
